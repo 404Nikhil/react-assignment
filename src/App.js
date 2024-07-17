e9292de887d0e55956a61a8d7a7eab3ab68fca13
404Nikhil <nicksd111@gmail.com>
2024-07-17 19:53:07 +0530

    auth and contexts

--- src/App.js
+++ src/App.js
@@ -1,24 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { BrowserRouter as Router, Route, Switch, Redirect } from 'react-router-dom';
+import { AuthProvider } from './contexts/AuthContext';
+import Login from './components/Auth/Login';
+import SignUp from './components/Auth/SignUp';
+import TrackingScreen from './components/TrackingScreen';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <AuthProvider>
+      <Router>
+        <Switch>
+          <Route path="/login" component={Login} />
+          <Route path="/signup" component={SignUp} />
+          <Route path="/tracking" component={TrackingScreen} />
+          <Redirect from="/" to="/login" />
+        </Switch>
+      </Router>
+    </AuthProvider>
   );
 }
 
